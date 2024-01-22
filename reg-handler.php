833a7ce3ce4f8b943f2d18cc04d5d62e09ac49a4
Yuriy <focus.us3r@gmail.com>
2024-01-22 22:34:39 +0100

    created validation and creating data of registration to file

--- reg-handler.php
+++ reg-handler.php
@@ -1,11 +1,30 @@
 <?php
 
-if ('POST' !== $_SERVER['REQUEST_METHOD']){
+if ('POST' !== $_SERVER['REQUEST_METHOD']) {
     // header('HTTP/1.1 405 Method Not Allowed');
     http_response_code(405);
     exit;
-} else {
-    echo '<div style="margin: 0; display: flex; align-items: center; justify-content: center; height: 100vh;"><b>
-    All is OK</b>
-    </div>';
 }
+
+$email = trim($_POST['email']); // trim extra characters
+$pass = md5($_POST['password']); // hash password
+
+
+// check user existence 
+$filename = "./storage/users/$email.json";
+if (is_file($filename)) {
+    http_response_code(302);
+    exit('User already exists!');
+}
+
+
+// create user
+$user = [
+    'email' => $email,
+    'password' => $pass
+];
+
+
+$contents = json_encode($user, JSON_PRETTY_PRINT);
+file_put_contents($filename, $contents);
+echo "User $email was created successfully!";
